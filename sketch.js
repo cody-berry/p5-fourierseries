1077ba26379e47083192a7d420b814c34587ec41
Cody <cody.tian@icloud.com>
2021-10-06 12:47:08 -0700

    add circle, moving line and point

--- sketch.js
+++ sketch.js
@@ -6,10 +6,13 @@ code planning:
     circle, point, and line
     second circle on first
     for loop on n
+    wave
     n slider
     sawtooth wave
  */
 let font
+let time = 0
+let r
 
 function preload() {
     font = loadFont('fonts/Meiryo-01.ttf')
@@ -22,4 +25,17 @@ function setup() {
 
 function draw() {
     background(209, 80, 30)
+    translate(width/4, height/2)
+    r = 100
+    noFill()
+    stroke(0, 0, 100)
+    circle(0, 0, r*2)
+    // now let's draw the point! First, we need to define where it is.
+    let x = r*cos(time/100)
+    let y = r*sin(time/100)
+    // Then, we can draw a line from the center of our circle to our point,
+    // which is actually going to be another circle.
+    line(0, 0, x, y)
+    circle(x, y, r/2)
+    time++
 }
