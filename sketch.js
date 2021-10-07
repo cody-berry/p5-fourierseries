1e4f7fcc95266c01555ffae9455c5db44ceaa124
Cody <cody.tian@icloud.com>
2021-10-07 10:26:48 -0700

    add second circle on first

--- sketch.js
+++ sketch.js
@@ -12,7 +12,8 @@ code planning:
  */
 let font
 let time = 0
-let r
+let r = 100
+let n = -1
 
 function preload() {
     font = loadFont('fonts/Meiryo-01.ttf')
@@ -26,16 +27,28 @@ function setup() {
 function draw() {
     background(209, 80, 30)
     translate(width/4, height/2)
-    r = 100
     noFill()
     stroke(0, 0, 100)
     circle(0, 0, r*2)
-    // now let's draw the point! First, we need to define where it is.
+    // now let's draw the point! First, we need to define where it is, and
+    // update n.
+    n+=2
     let x = r*cos(time/100)
     let y = r*sin(time/100)
     // Then, we can draw a line from the center of our circle to our point,
     // which is actually going to be another circle.
     line(0, 0, x, y)
-    circle(x, y, r/2)
+    r /= 4
+    circle(x, y, r*2)
+    // Now let's draw another circle!
+    n+=2
+    translate(x, y)
+    x = r*cos(n*time/100)
+    y = r*sin(n*time/100)
+    line(0, 0, x, y)
+    r /= 4
+    circle(x, y, r*2)
     time++
+    r = 100
+    n=-1
 }
