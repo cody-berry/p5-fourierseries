4cb5fd245ed3d59fd6e1e36323761decbf769e1b
Cody <cody.tian@icloud.com>
2021-10-07 10:33:33 -0700

    put n in a for loop

--- sketch.js
+++ sketch.js
@@ -3,16 +3,17 @@
 @date 2021-10-06
 
 code planning:
-    circle, point, and line
-    second circle on first
-    for loop on n
+.   circle, point, and line
+.   second circle on first
+.   for loop on n
     wave
     n slider
     sawtooth wave
  */
 let font
 let time = 0
-let r = 100
+let originalR = 25
+let r
 let n = -1
 
 function preload() {
@@ -29,26 +30,19 @@ function draw() {
     translate(width/4, height/2)
     noFill()
     stroke(0, 0, 100)
-    circle(0, 0, r*2)
-    // now let's draw the point! First, we need to define where it is, and
-    // update n.
-    n+=2
-    let x = r*cos(time/100)
-    let y = r*sin(time/100)
-    // Then, we can draw a line from the center of our circle to our point,
-    // which is actually going to be another circle.
-    line(0, 0, x, y)
-    r /= 4
-    circle(x, y, r*2)
-    // Now let's draw another circle!
-    n+=2
-    translate(x, y)
-    x = r*cos(n*time/100)
-    y = r*sin(n*time/100)
-    line(0, 0, x, y)
-    r /= 4
-    circle(x, y, r*2)
+    r = 4*originalR
+    // let's put n in a for loop!
+    for (let n = 1; n < 10; n+=2) {
+        circle(0, 0, r * 2)
+        // now let's draw the point! First, we need to define where it is, and
+        // update n.
+        let x = r * cos(n*time / 100)
+        let y = r * sin(n*time / 100)
+        // Then, we can draw a line from the center of our circle to our point,
+        // which is actually going to be another circle.
+        line(0, 0, x, y)
+        translate(x, y)
+        r = 4*originalR/n
+    }
     time++
-    r = 100
-    n=-1
 }
