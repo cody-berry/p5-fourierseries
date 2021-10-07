11576ef971509fe0b6c903a92b3748fecb5609be
Cody <cody.tian@icloud.com>
2021-10-07 12:06:04 -0700

    make speed a slider, improve sliders

--- sketch.js
+++ sketch.js
@@ -7,16 +7,17 @@ code planning:
 .   second circle on first
 .   for loop on n
 .   wave
-    n slider
-    sawtooth wave
+.   n slider
+    ?sawtooth wave
  */
 let font
 let time = 0
-let originalR = 10
+let originalR = 20
 let r
 let n = -1
 let wave = []
 let maxN
+let speed
 
 function preload() {
     font = loadFont('fonts/Meiryo-01.ttf')
@@ -24,9 +25,12 @@ function preload() {
 
 function setup() {
     createCanvas(640, 360)
-    colorMode(HSB, 360, 100, 100, 0)
-    maxN = createSlider(10, 100, 10, 5)
+    colorMode(HSB, 360, 100, 100, 100)
+    maxN = createSlider(1, 21)
+    speed = createSlider(0.01, 0.5, 0.02, 0.01)
 }
+
+console.log("🐳")
 
 function draw() {
     background(209, 80, 30)
@@ -37,31 +41,42 @@ function draw() {
     let x=0
     let y=0
     let prevX, prevY
+
     // let's put n in a for loop!
-    for (let n = 1; n < maxN.value(); n+=0) {
+    for (let i=0; i < maxN.value(); i+=1) {
+
+        let n = 2*i + 1
         prevX = x
         prevY = y
+        r = 4*originalR/(n)
+        stroke(0, 0, 100, 30)
         circle(prevX, prevY, r * 2)
+
         // now let's draw the point! First, we need to define where it is, and
         // update n.
-        x += r * cos(n*time / 100)
-        y += r * sin(n*time / 100)
+        x += r * cos(n*time)
+        y += r * sin(n*time)
+
         // Then, we can draw a line from the center of our circle to our point,
         // which is actually going to be another circle.
+
+        stroke(0, 0, 100)
         line(prevX, prevY, x, y)
-        n+=2
-        r = 4*originalR/(n)
     }
+
     wave.unshift(y)
+    line(x, y, 12*originalR, wave[0])
+
     // we'll want to draw the wave
     beginShape()
-    for (let i = 0; i < wave.length; i++) {
-        vertex(i+16*originalR, wave[i])
-    }
+
+    stroke(0, 0, 100)
+    for (let i = 0; i < wave.length; i++)
+        vertex(i+12*originalR, wave[i])
     endShape()
-    line(x, y, 16*originalR, wave[0])
-    if (wave.length > 315) {
+
+    if (wave.length > 315)
         wave.pop()
-    }
-    time++
+
+    time += speed.value()
 }
